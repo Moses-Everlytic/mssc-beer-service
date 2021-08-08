5358d80114c2169d616ccebe076308647c75f6fa
Moses <katsande.moses@gmail.com>
2021-08-08 18:52:33 +0200

    Refactor

--- src/main/java/com/nexstudio/msscbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/com/nexstudio/msscbeerservice/web/model/BeerStyleEnum.java
@@ -1,5 +1,5 @@
 package com.nexstudio.msscbeerservice.web.model;
 
 public enum BeerStyleEnum {
-   LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, PALE_LAGER, SAISON, WITBIER,  
+   LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, PALE_LAGER, SAISON, WITBIER
 }
