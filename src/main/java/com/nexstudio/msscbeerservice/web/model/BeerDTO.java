561a597e6849fc90de55038d598eb4a0fcb2e011
Moses <katsande.moses@gmail.com>
2021-09-07 21:10:50 +0200

    Added Validation Constraints

--- src/main/java/com/nexstudio/msscbeerservice/web/model/BeerDTO.java
+++ src/main/java/com/nexstudio/msscbeerservice/web/model/BeerDTO.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import com.nexstudio.msscbeerservice.constants.BeerStyleEnum;
 
 import lombok.AllArgsConstructor;
@@ -17,15 +22,30 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDTO {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
+
+    @Positive
+    @NotNull
     private Long upc;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModified;
 }
